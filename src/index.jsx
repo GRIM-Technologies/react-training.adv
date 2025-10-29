3fe7bb6fb6d31adb58657e2a606403fbf88ce5f5
Andre Drougge <drougge@Andres-MacBook-Pro.local>
2025-10-29 21:09:41 +0200

    Add rudimentary implementation of redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './styles/index.css';
+
+import userReducer from './reducers/userReducer';
 
 // Layout
 import Layout from './components/Layout';
@@ -12,20 +16,27 @@ import Concurrent from './pages/Concurrent';
 import State from './pages/State';
 import Hooks from './pages/Hooks';
 
+let store = createStore(
+  userReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="concurrent" element={<Concurrent />} />
-          <Route path="state" element={<State />} />
-          <Route path="hooks" element={<Hooks />} />
-          {/* Catch all - replace with a NotFound component if desired */}
-          <Route path="*" element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="concurrent" element={<Concurrent />} />
+            <Route path="state" element={<State />} />
+            <Route path="hooks" element={<Hooks />} />
+            {/* Catch all - replace with a NotFound component if desired */}
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
