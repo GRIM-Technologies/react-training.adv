12d1b51c0286f7595841d2f0ae8e42c3160787fc
benedicandra18 <benedicandra18@stud.ase.com>
2025-10-30 14:56:41 +0200

    add store provider into app

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import appReducer from './reducers/appReducer';
+
 import './styles/index.css';
 
 // Layout
@@ -11,19 +15,23 @@ import Home from './pages/Home';
 import Concurrent from './pages/Concurrent';
 import State from './pages/State';
 
+let store = createStore(appReducer);
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="concurrent" element={<Concurrent />} />
-          <Route path="state" element={<State />} />
-          {/* Catch all - replace with a NotFound component if desired */}
-          <Route path="*" element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="concurrent" element={<Concurrent />} />
+            <Route path="state" element={<State />} />
+            {/* Catch all - replace with a NotFound component if desired */}
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
